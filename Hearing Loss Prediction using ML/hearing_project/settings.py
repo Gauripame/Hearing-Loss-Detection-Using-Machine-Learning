62edd178c1caa2d9b44bc48c62237379ac3ed4fa
Gauri Pame <139797116+Gauripame@users.noreply.github.com>
2026-01-05 13:47:05 +0530

    Update settings.py

--- Hearing Loss Prediction using ML/hearing_project/settings.py
+++ Hearing Loss Prediction using ML/hearing_project/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'hearing_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.mysql',
@@ -96,7 +96,13 @@ DATABASES = {
 	'PORT': '3306'
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -163,3 +169,4 @@ MESSAGE_TAGS = {
     messages.ERROR : 'alert-danger',
 }
 Fix ALLOWED_HOSTS
+Use SQLite for Render
