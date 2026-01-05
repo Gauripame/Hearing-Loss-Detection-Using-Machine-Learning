0ba2c1635398625ea830d4e803734f66fe3c3e9e
Gauri Pame <139797116+Gauripame@users.noreply.github.com>
2026-01-05 16:35:06 +0530

    Clean up comments in settings.py
    Removed unnecessary comment block from settings.py

--- Hearing Loss Prediction using ML/hearing_project/settings.py
+++ Hearing Loss Prediction using ML/hearing_project/settings.py
@@ -85,7 +85,6 @@ WSGI_APPLICATION = 'hearing_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-"""
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.mysql',
@@ -103,7 +102,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
